a790b8fe723a19258e978fb3fc78a09280eae827
Timur Giniyatov <timur_3107@bk.ru>
2022-04-20 22:03:06 +0500

    fix: открытие попапа на enter

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,22 @@
-let popup = document.querySelector(".popup")
-let popupButtonCave = popup.querySelector(".popup__saveButton")
+const popup = document.querySelector(".popup")
+const popupButtonSave = popup.querySelector(".popup__saveButton")
 
-let formElement = popup.querySelector(".popup__form");
+const formElement = popup.querySelector(".popup__form");
 
-let nameInput = formElement.querySelector(".popup__input-name");
-let jobInput = formElement.querySelector(".popup__input-job");
+const nameInput = formElement.querySelector(".popup__input-name");
+const jobInput = formElement.querySelector(".popup__input-job");
 
-let profile = document.querySelector(".profile")
-let profileGeneral = profile.querySelector(".profile__general")
+const profile = document.querySelector(".profile")
+const profileName = profile.querySelector(".profile__name")
+const profileAbout = profile.querySelector(".profile__about")
 
-let profileButtonEdit = profile.querySelector(".profile__editButton")
-let popupButtonClose = popup.querySelector(".popup__closeButton")
+const profileButtonEdit = profile.querySelector(".profile__editButton")
+const popupButtonClose = popup.querySelector(".popup__closeButton")
 
 // открытие попапа
 function pressedEdit () {
-  nameInput.value = "Жак-Ив Кусто";
-  jobInput.value = "Исследователь океана";
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileAbout.textContent;
   popup.classList.add("popup_opened")
 }
 
@@ -30,14 +31,9 @@ popupButtonClose.addEventListener("click",pressedClosePopup)
 // изменение form
 function pressedSavePopup (evt) {
   evt.preventDefault();
-  profileGeneral.innerHTML = `
-  <div class="profile__container">
-  <h1 class="profile__name">${nameInput.value}</h1>
-  <button class="profile__editButton"></button>
-  </div>
-  <p class="profile__about">${jobInput.value}</p>`
-  popup.classList.remove("popup_opened")
+  profileName.textContent = nameInput.value
+  profileAbout.textContent = jobInput.value
+  pressedClosePopup()
 }
 
-popupButtonCave.addEventListener("click",pressedSavePopup)
-
+formElement.addEventListener("submit",pressedSavePopup)
