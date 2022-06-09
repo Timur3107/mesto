b202fd14f4bfa93843d1a3d49d09e63e0a9d6c6c
Timur Giniyatov <timur_3107@bk.ru>
2022-06-09 17:27:13 +0500

    hotfix: исправление ошибок ревью ПР5

---
+++ scripts/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Эльбрус',
+    link: 'images/Elbrus.jpeg'
+  },
+  {
+    name: 'Чегет',
+    link: 'https://images.unsplash.com/photo-1577978728453-3d444d6f96da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+  },
+  {
+    name: 'ГЛЦ Абзаково',
+    link: 'images/abzakovo.jpg'
+  },
+  {
+    name: 'ГЛЦ Озеро Банное',
+    link: 'images/bannoe.jpg'
+  },
+  {
+    name: 'ГЛЦ Мраткино',
+    link: 'images/mratkino.jpg'
+  },
+  {
+    name: 'Зирган Тау',
+    link: 'https://images.unsplash.com/photo-1603454211489-162865d96cc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+  }
+];
