c681581344a11c881720dea92de8a60889d19791
Timur Giniyatov <timur_3107@bk.ru>
2022-06-11 03:04:04 +0500

    hotfix: небольшие поправки и сортировка js

--- scripts/index.js
+++ scripts/index.js
@@ -21,54 +21,29 @@ const profileAbout = profile.querySelector(".profile__about")
 // кнопки открытия попапов
 const buttonProfileEdit = profile.querySelector(".profile__edit-button")
 const buttonAddCard = document.querySelector(".profile__add-button")
+
 // кнопки закрытия попапов
 const buttonPopupClose = document.querySelector(".popup__close-button")
 const buttonCloseCard = popupAddCard.querySelector(".popup__close-button")
 const buttonClosePicture = popupViewPicture.querySelector(".popup__close-button")
 
-// открытие попапа (1)
-function openPopup (popup) {
-  popup.classList.add("popup_opened")
-}
-buttonProfileEdit.addEventListener("click", event =>{
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileAbout.textContent;
-  openPopup(popupEditProfile)
-})
-
-// закрытие попапа (1)
-function closePopup (popup) {
-  popup.classList.remove("popup_opened")
-}
-buttonPopupClose.addEventListener("click",event =>{
-  closePopup(popupEditProfile)
-})
-
-// отправка form (1)
-function savePopupEditProfile (evt) {
-  evt.preventDefault();
-  profileName.textContent = nameInput.value
-  profileAbout.textContent = jobInput.value
-  closePopup(popupEditProfile)
-}
-formElement.addEventListener("submit",savePopupEditProfile)
-
-
-// второй попап
-
 // карточки
 const cards = document.querySelector(".elements")
 const cardTemplate = document.querySelector("#template-element").content
-const card = cardTemplate.querySelector(".element").cloneNode(true)
 
-// открытие попапа (2)
-buttonAddCard.addEventListener("click",event =>{
-  openPopup(popupAddCard)
-})
-// закрытие попапа (2)
-buttonCloseCard.addEventListener("click",event =>{
-  closePopup(popupAddCard)
-})
+const imagePopup = document.querySelector('.popup__image')
+const titlePicturePopup = document.querySelector('.popup__title-picture')
+
+// функции
+// открытие попапа 1 и 2
+function openPopup (popup) {
+  popup.classList.add("popup_opened")
+}
+
+// закрытие попапа 1 и 2
+function closePopup (popup) {
+  popup.classList.remove("popup_opened")
+}
 
 // listeners
 function listenActions(element){
@@ -78,27 +53,21 @@ function listenActions(element){
 }
 
 // обработка карточки
-function renderСards (name, link){
+function renderСard (newCard){
+  listenActions(newCard)
+  cards.prepend(newCard)
+}
+
+// создание карточки
+function createCard(name,link) {
   const newCard = cardTemplate.cloneNode(true)
   const newCardTitle = newCard.querySelector(".element__title")
   const newCardImage = newCard.querySelector(".element__image")
   newCardTitle.textContent = name
   newCardImage.alt = name
   newCardImage.src = link
-
-  listenActions(newCard)
-  cards.prepend(newCard)
+  renderСard(newCard)
 }
-
-// отправка form (2)
-function savePopupAddCard (evt) {
-  evt.preventDefault();
-  renderСards(nameInputCard.value,linkInputCard.value)
-  nameInputCard.value = ""
-  linkInputCard.value = ""
-  closePopup(popupAddCard)
-}
-formElementCard.addEventListener("submit",savePopupAddCard)
 
 // лайк карточки
 function likeCard (evt){
@@ -110,18 +79,25 @@ function deleteCard (evt){
   evt.target.closest(".element").remove();
 }
 
-// обработка массива
-initialCards.reverse().forEach((element)=>{
-  renderСards(element.name, element.link)
-  // cards.append(newCard)
-})
+// отправка form (1)
+function savePopupEditProfile (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value
+  profileAbout.textContent = jobInput.value
+  closePopup(popupEditProfile)
+}
 
-// третий попап
+// отправка form (2)
+function savePopupAddCard (evt) {
+  evt.preventDefault();
+  createCard(nameInputCard.value,linkInputCard.value)
+  nameInputCard.value = ""
+  linkInputCard.value = ""
+  closePopup(popupAddCard)
+}
 
 // открытие попапа (3)
 function viewImage(evt) {
-  const imagePopup = document.querySelector('.popup__image')
-  const titlePicturePopup = document.querySelector('.popup__title-picture')
   imagePopup.alt = evt.target.alt
   imagePopup.src = evt.target.src
   titlePicturePopup.textContent = evt.target.alt
@@ -129,7 +105,38 @@ function viewImage(evt) {
   openPopup(popupViewPicture)
 }
 
+
+// слушатели
+// открытие попапа (1)
+buttonProfileEdit.addEventListener("click", event =>{
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileAbout.textContent;
+  openPopup(popupEditProfile)
+})
+
+// закрытие попапа (1)
+buttonPopupClose.addEventListener("click",event =>{
+  closePopup(popupEditProfile)
+})
+
+// открытие попапа (2)
+buttonAddCard.addEventListener("click",event =>{
+  openPopup(popupAddCard)
+})
+// закрытие попапа (2)
+buttonCloseCard.addEventListener("click",event =>{
+  closePopup(popupAddCard)
+})
 // закрытие попапа (3)
 buttonClosePicture.addEventListener("click", event =>{
   closePopup(popupViewPicture)
+})
+
+// формы
+formElement.addEventListener("submit",savePopupEditProfile)
+formElementCard.addEventListener("submit",savePopupAddCard)
+
+// обработка массива
+initialCards.reverse().forEach((element)=>{
+  createCard(element.name, element.link)
 })
